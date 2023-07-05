6831f0b9f6f79dd051060ff010f8611074cbb145
闫云琦 <yyq22@mails.tsinghua.edu.cn>
2023-07-05 16:02:24 +0800

    notes of main.m

--- main.m
+++ main.m
@@ -2,13 +2,15 @@ define_constants;
 % take 33 bus distribution system as an example
 mpc = IEEE33;
 % Break lines setup
-break_lines = [1];
-
+break_lines = [3, 5, 7, 9 ,11];
 z = ones(size(mpc.branch, 1), 1);
 z(break_lines) = 0.;
 
+% Optimization problem
 [Pf, Qf, u, Pg, Qg, Pl, Ql, c, delta, epsilon] = resilient_reconfiguration(mpc, z);
 
+% Visualization of load shedding
 plot(value(Pl)); hold on; plot(mpc.bus(:, PD) ./ mpc.baseMVA); hold off;
 
+% Visualization of topology
 plotCase33(z, value(c), mpc.gen(:, GEN_BUS), value(epsilon), value(delta));
